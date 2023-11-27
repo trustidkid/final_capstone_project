2310e4e5db128bf1b1e384db23e0146b2647ac95
semiu biliaminu <39337003+trustidkid@users.noreply.github.com>
2023-11-27 19:29:57 +0100

    modifed api endpoints
    switch the api from rapid to socrata fema data

---
+++ dags/gcs_to_bigquery.py
@@ -0,0 +1,42 @@
+import os
+from google.cloud import bigquery
+
+
+def gcs_to_bigquery(bucket_name, file_path):
+
+    
+
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    relative_path = 'google/adeairflow-6fe91e99ed98.json'
+    absolute_path = os.path.join(script_directory, relative_path)
+
+    # BigQuery client
+    client = bigquery.Client.from_service_account_json(absolute_path)
+    print("********** got here ***************")
+    # bucket and file path
+    bucket_name = bucket_name # os.getenv['GCP_GCS_BUCKET']
+    file_path = file_path  # os.getenv['GCP_file'] #'car_sale'
+   
+    # Specify BigQuery dataset and table
+    dataset_id = "fema_disaster" #os.getenv['DATA_SET']
+    table_id = 'disasters'
+
+    # Construct the GCS URI
+    uri = f'gs://{bucket_name}/{file_path}'
+    # uri = f'gs://us_car_sale/car_sale'
+
+    # Configure the load job
+    job_config = bigquery.LoadJobConfig(
+        source_format=bigquery.SourceFormat.CSV,
+        skip_leading_rows=1,  # Adjust based on your data file
+        autodetect=True,  # Set to False if you have a schema defined
+    )
+    print("********* connected to bigquery succesfully  **************")
+    # Load data from GCS to BigQuery
+    load_job = client.load_table_from_uri(uri, dataset_id + '.' + table_id, job_config=job_config)
+
+    # Wait for the job to complete
+    load_job.result()
+
+    # Print the result
+    print(f'Loaded {load_job.output_rows} rows to {dataset_id}.{table_id}')
---
+++ dags/postgres_to_gcs.py
@@ -0,0 +1,41 @@
+import psycopg2
+from google.cloud import storage
+import os
+
+def to_gcs(user, password, host, db, table_name ):
+    # Connect to PostgreSQL
+    conn = psycopg2.connect(
+        host= host,
+        database=db,
+        user= user,
+        password=password
+    )
+
+    # Execute SQL query to extract data
+    query = "SELECT * FROM " + table_name
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+    # Fetch the data
+    data = cursor.fetchall()
+
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    relative_path = 'google/adeairflow-6fe91e99ed98.json'
+    absolute_path = os.path.join(script_directory, relative_path)
+
+    # Set up GCS client
+    client = storage.Client.from_service_account_json(absolute_path)
+
+    # GCS bucket and destination file name
+    bucket_name = 'us-car-sale'
+    blob_name = 'car-sale' 
+
+    # Upload data to GCS
+    bucket = client.get_bucket(bucket_name)
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string('\n'.join([','.join(map(str, row)) for row in data]))
+    
+    # Close the connection
+    conn.close()
+    
+    print("Data loaded successfully to GSC Bucket { }" + bucket_name)
